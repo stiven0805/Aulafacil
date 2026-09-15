140abd8f8e2c13da7601cdac9e4a6091e08466f9
stiven <134813199+stiven0805@users.noreply.github.com>
2026-09-14 21:41:11 -0500

    fix: requirements typos and add whitenoise middleware

--- backend/settings/base.py
+++ backend/settings/base.py
@@ -25,6 +25,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
